bb509defe896074db9601308ec0902d0dcd9bea6
kimiaki2510 <153046sk@gmail.com>
2020-04-19 09:52:36 +0900

    record

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,5 +1,10 @@
 class RecordsController < ApplicationController
+
   def index
+    if logged_in?
+      @record = current_user.records.build #form with用
+      @records = current_user.records.order(id: :desc).page(params[:page])
+    end
   end
 
   def show
