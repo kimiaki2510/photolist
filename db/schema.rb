b633cfa878d1000c656d77438d152633ca74fe86
kimiaki2510 <153046sk@gmail.com>
2020-05-03 10:50:28 +0900

    post

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_30_131600) do
+ActiveRecord::Schema.define(version: 2020_05_03_012944) do
 
   create_table "records", force: :cascade do |t|
     t.string "title"
     t.string "content"
-    t.integer "user_id_id"
     t.string "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.index ["user_id"], name: "index_records_on_user_id"
-    t.index ["user_id_id"], name: "index_records_on_user_id_id"
   end
 
   create_table "users", force: :cascade do |t|
