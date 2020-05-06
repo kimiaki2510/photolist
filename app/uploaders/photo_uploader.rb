baa47f2f64ebfd59eacd839e2beccf694bac3f3e
kimiaki2510 <153046sk@gmail.com>
2020-05-06 20:02:55 +0900

    user image

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -21,6 +21,10 @@ class PhotoUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
+  #画像の上限を200px
+  process :resize_to_limit => [100,100]
+  #保存形式をJPGにする
+  process :convert => 'jpg'
   # Process files as they are uploaded:
   # process scale: [200, 300]
   #
@@ -29,15 +33,16 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  version :thumb do
+    process resize_to_fit: [50, 50]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  #jpg, jpeg, gif, pngしか受け付けない
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
