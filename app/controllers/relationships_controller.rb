456de8f827db40579d31d4bc3f77a16f750a9ef3
kimiaki2510 <153046sk@gmail.com>
2020-05-10 09:20:11 +0900

    before follow

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,16 +2,20 @@ class RelationshipsController < ApplicationController
   before_action :require_user_logged_in
 
   def create
-    user = User.find(params[:follow_id])
-    current_user.follow(user)
-    flash[:success] = 'フォローしました'
-    redirect_to user
+    @user = User.find(params[:follow_id])
+    current_user.follow(@user)
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 
   def destroy
-    user = User.find(params[:follow_id])
-    current_user.unfollow(user)
-    flash[:success] = 'フォローを解除しました'
-    redirect_to user
+    @user = User.find(params[:follow_id])
+    current_user.unfollow(@user)
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 end
