f6947e640447bbabbe2a4bfab6eb458d83383ceb
kimiaki2510 <153046sk@gmail.com>
2020-06-22 21:14:20 +0900

    record like

--- app/models/record.rb
+++ app/models/record.rb
@@ -2,8 +2,6 @@ class Record < ApplicationRecord
   belongs_to :user
   has_many :likes
   has_many :iine_users, through: :likes, source: :user
-  #has_many :liked_users, through: :likes, source: :user
-
 
   mount_uploader :photo, PhotoUploader
   validates :content, presence: true, length: {maximum: 255}
