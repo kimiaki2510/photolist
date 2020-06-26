ee020fda5739fd6cf1a55638269ac877c5f8a459
kimiaki2510 <153046sk@gmail.com>
2020-06-26 23:19:31 +0900

    like refactoring

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,8 +3,11 @@ class LikesController < ApplicationController
 
   def create
     #byebug
-    @like = current_user.likes.create(record_id: params[:record_id])
-    redirect_to root_path
+    @like = current_user.likes.create(record_id: params[:record_id])    
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js
+    end
 
     #@record = Record.find(params[:id])
     #unless @record.iine?(current_user)
@@ -19,7 +22,13 @@ class LikesController < ApplicationController
   def destroy
     @like = Like.find_by(record_id: params[:record_id], user_id: @current_user.id)
     @like.destroy
-    redirect_to root_path
+
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js
+    end
+
+
     #byebug
     #@record = Like.find(params[:id]).record
     #@record = Record.find(params[:id])
