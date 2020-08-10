4a4f2deadb74a6957498636f6b41df1a5fb34954
kimiaki2510 <153046sk@gmail.com>
2020-08-10 09:59:42 +0900

    fav

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,6 @@
 class LikesController < ApplicationController
   before_action :require_user_logged_in
+  before_action :set_record
 
 #いいね
   def create
@@ -12,7 +13,7 @@ class LikesController < ApplicationController
 
 #いいねを外す
   def destroy
-    like = current_user.likes.find_by(record_id: params[:record_id])
+    like = current_user.likes.find_by(record_id: params[:record_id], user_id: current_user.id)
     like.destroy
     respond_to do |format|
       format.html { redirect_to root_path }
@@ -21,4 +22,8 @@ class LikesController < ApplicationController
   end
 
   private
+
+  def set_record
+    @record = Record.find(params[:record_id])
+  end
 end
