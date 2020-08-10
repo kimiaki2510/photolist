cc1ba8b5dd78eb8214a7967a42b389114dd85e10
kimiaki2510 <153046sk@gmail.com>
2020-08-10 10:04:25 +0900

    fav_comtroller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,20 +5,14 @@ class LikesController < ApplicationController
 #いいね
   def create
     @like = current_user.likes.create(record_id: params[:record_id])
-    respond_to do |format|
-      format.html { redirect_to root_path }
-      format.js
-    end
+    @record.reload
   end
 
 #いいねを外す
   def destroy
     like = current_user.likes.find_by(record_id: params[:record_id], user_id: current_user.id)
     like.destroy
-    respond_to do |format|
-      format.html { redirect_to root_path }
-      format.js
-    end
+    @record.reload
   end
 
   private
